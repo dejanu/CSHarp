986fca8e76da1d53158445e520638143a22d39d2
Dej <dejanualex@yahoo.com>
2018-02-19 15:12:59 +0200

    Create OOP.cs

---
+++ OOP.cs
@@ -0,0 +1,66 @@
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+            
+            //create animal
+            Animal fox = new Animal();
+           
+            fox.MakeSound();
+            fox.sound= "dksad";
+            fox.name="dsasadasd";
+            fox.MakeSound();
+            
+            //call static method
+             Console.WriteLine(Animal.GetAnimal());
+            
+                Animal wolf = new Animal();
+            
+            //call static method
+             Console.WriteLine(Animal.GetAnimal());
+           
+        }
+    }
+    
+    
+    
+    public class Animal {
+            public string name;
+            public string sound;
+            public static int counter = 0;
+        
+        public Animal(string name = "No name", string sound = "No sound"){
+            
+            //name = n 
+            this.name = name;
+            this.sound = sound;
+            counter++;
+            
+   
+            
+    
+    }
+        public void MakeSound(){
+              Console.WriteLine(name);  
+              Console.WriteLine(sound);
+        }
+        
+        public static int GetAnimal(){
+                return counter;
+        }
+
+    
+    }//class
+}
